954d60611a1879f05bf890f5ea9cd69751aa287e
Vineet Reddy <hotshot2797@gmail.com>
2019-04-19 14:55:38 +0530

    Started EC

---
+++ ec.py
@@ -0,0 +1,11 @@
+p = 17
+a = 0
+b = 7
+
+infinity = [-1,-1]
+
+def addition(arg1,arg2):
+    if(arg1==-1,-1 and arg2==-1,-1): #This is the infinity added to infinity
+        return arg1
+
+print(addition(infinity,infinity))
