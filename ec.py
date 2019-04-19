4d58447d202338248640e03c6fc2c5d6d032d4b4
Vineet Reddy <hotshot2797@gmail.com>
2019-04-19 18:02:57 +0530

    Point double works

--- ec.py
+++ ec.py
@@ -1,11 +1,69 @@
-p = 17
-a = 0
-b = 7
+import numpy as np
+import math
+p = 5
+a = 4
+b = 4
 
 infinity = [-1,-1]
+def inv(e,phi):
+    def ext_GCD(e_KEY, mod_PHI):
+        if (e_KEY == 0):
+            return (mod_PHI, 0, 1)
+        g, y, x = ext_GCD(mod_PHI%e_KEY,e_KEY)
+        return (g, x - (mod_PHI//e_KEY) * y, y)
+
+    def modinv(e_KEY, mod_PHI):
+        g, x, y = ext_GCD(e_KEY, mod_PHI)
+        return x%mod_PHI
+    d_key = modinv(e,phi)
+    return d_key
 
 def addition(arg1,arg2):
-    if(arg1==-1,-1 and arg2==-1,-1): #This is the infinity added to infinity
+    if(arg1==[-1,-1] and arg2==[-1,-1]): #This is the infinity added to infinity
         return arg1
+    '''
+    This next construct is for when a point is added to the point
+    at infinity
+    '''
+    if(arg1==[-1,-1]):
+        return arg2
+    if(arg2==[-1,-1]):
+        return arg1
+    '''
+    Same X coordinate
+    '''
+    if(arg1[0]==arg2[0]):
+        return infinity
 
-print(addition(infinity,infinity))
+    if(arg1[0]!=arg2[0]):
+        inverse = inv(arg2[0]-arg1[0],p)
+        slope = int(arg2[1]-arg1[1])*inverse
+        slope = int(slope)
+        x_r = int((pow(slope,2) - arg1[0] - arg2[0])%p)
+        y_r = (slope*(arg1[0]-x_r) - arg1[1])%p
+        return [x_r,y_r]
+
+
+def pointDoubling(arg1):
+    inverse = inv(2*arg1[1],p)
+    slope = (3*pow(arg1[0],2)+a)*inverse
+    # slope=math.ceil(slope)
+    x_r = (int(pow(slope,2)-2*arg1[0]))%p
+    y_r = (int(slope*(arg1[0]-x_r)-arg1[1]))%p
+    # arg1[0] = x_r
+    # arg1[1] = y_r
+    return [x_r,y_r]
+
+def multiplePoint(numbers,times):
+    for i in range(0,times):
+        newNumbers = pointDoubling([numbers])
+print(pointDoubling([1,3]))
+
+# print(addition([1,3],[4,2]))
+
+
+def functionOutput(x):
+    y = (x**3+a*x+b)%p
+    return int(math.sqrt(y))
+# print(functionOutput(1))
+
